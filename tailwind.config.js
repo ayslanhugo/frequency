a096f8f9abf25171c2e66ddb164f2b8bdbf01c85
ayslanhugo <ayslanhugo1@gmail.com>
2025-06-08 15:00:52 -0300

    Tela about funcionando

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
   ],
   darkMode: 'class',
   theme: {
